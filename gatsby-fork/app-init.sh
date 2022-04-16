89a50796abd13830d11a2599c839f9de8ac5d7d1
Dukka-De-La-Dokka <59502111+Dukka-De-La-Dokka@users.noreply.github.com>
2022-04-16 18:24:39 +0900

    base-version-gatsby-fork

---
+++ gatsby-fork/app-init.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Execute this script upon attaching to the container
+# Cloning original repo into local device with only latest commit, then push them into your own remote repo
+
+# Getting variables from external txt file
+. ./variables.txt
+# Store git-credentials at container home dir upon first time git authorization
+git config --global credential.helper store
+# Git user info set globally prior to gatsby new installation
+git config --global user.name "$GIT_UNAME"
+git config --global user.email "$GIT_UMAIL"
+# npx cache clear
+npx -y clear-npx-cache
+# Launch project without installing gatsby-cli
+npx -y gatsby new "$REPODIR" "$RMT_REPO_ADDR"
+# Move to app repository
+cd "$REPODIR" || exit 1
+# If current branch is not main, change its name to main
+if [ "$($SHOW_CRB_CMD)" != "main" ]; then
+    git branch -m "$($SHOW_CRB_CMD)" main
+fi
+# Add remote and push to your own remote repo
+git remote add origin "$MY_REPO_ADDR"
+git push -u origin main
